444191eb6b99538a7467f47456150593108238f2
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-04 19:11:28 -0300

    Adicionar arquivos de projeto.

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Seu nome completo é: " + nomeCompleto);
+            Console.ReadKey();
+        }
+    }
+}
