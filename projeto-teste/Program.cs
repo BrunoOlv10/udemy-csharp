1bee7fddf450b3d06b30cbbe1f41cf7d890dc087
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-07 21:45:11 -0300

    add new exercise

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,42 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Console.Write("Insira o seu nome completo: ");
-            string nomeCompleto = Console.ReadLine();
+            Console.Write("Insira o primeiro valor: ");
+            string valor1 = Console.ReadLine();
+            int numero1 = int.Parse(valor1);
 
-            Console.WriteLine("Seu nome completo é: " + nomeCompleto);
+            Console.WriteLine();
+
+            Console.Write("Insira o segundo valor: ");
+            string valor2 = Console.ReadLine();
+            int numero2 = int.Parse(valor2);
+
+            Console.WriteLine();
+
+            int soma = numero1 + numero2;
+            Console.WriteLine("Soma: " + soma);
+
+            int subtracao = numero1 - numero2;
+            Console.WriteLine("Subtração: " + subtracao);
+
+            int multiplicacao = numero1 * numero2;
+            Console.WriteLine("Multiplicação: " + multiplicacao);
+
+            int divisao = numero1 / numero2;
+            Console.WriteLine("Divisão: " + divisao);
+
+            int modulo = numero1 % numero2;
+            Console.WriteLine("Modulo: " + modulo);
+
+            Boolean igual = numero1 == numero2;
+            Console.WriteLine("Iguais? " + igual);
+
+            Boolean diferentes = numero1 != numero2;
+            Console.WriteLine("Diferentes? " + diferentes);
+
+            Boolean comparacaoMaior = numero1 > numero2;
+            Console.WriteLine("Valor 1 é maior que valor 2? " + comparacaoMaior);
+
             Console.ReadKey();
         }
     }
