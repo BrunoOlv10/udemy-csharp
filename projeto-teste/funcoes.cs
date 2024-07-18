e2e225f87f835dfbf39537579acaafd912107528
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-18 12:16:07 -0300

    add array exercise

--- projeto-teste/funcoes.cs
+++ projeto-teste/funcoes.cs
@@ -8,63 +8,61 @@ namespace curso_udemy
 {
     internal class funcoes
     {
-        static void Main(string[] args)
-        {
-            // Calculadora
-            Console.Write("Digite o primeiro valor: ");
-            int numero1 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine();
+        //static void Main(string[] args)
+        //{
+        //    // Calculadora
+        //    Console.Write("Digite o primeiro valor: ");
+        //    int numero1 = Convert.ToInt16(Console.ReadLine());
+        //    Console.WriteLine();
 
-            Console.Write("Digite o segundo valor: ");
-            int numero2 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine();
+        //    Console.Write("Digite o segundo valor: ");
+        //    int numero2 = Convert.ToInt16(Console.ReadLine());
+        //    Console.WriteLine();
 
-            Console.Write("Digite um operador (+ - * /): ");
-            string operador = Console.ReadLine();
-            Console.WriteLine();
+        //    Console.Write("Digite um operador (+ - * /): ");
+        //    string operador = Console.ReadLine();
+        //    Console.WriteLine();
 
-                if (operador == "+")
-                {
-                    Console.WriteLine($"Resultado: {Somatoria(numero1, numero2)}");
-                }
-                else if (operador == "-")
-                {
-                    Console.WriteLine($"Resultado: {Subtracao(numero1, numero2)}");
-                }
-                else if (operador == "*")
-                {
-                    Console.WriteLine($"Resultado: {Multiplicacao(numero1, numero2)}");
-                }
-                if (operador == "/")
-                {
-                    Console.WriteLine($"Resultado: {Divisao(numero1, numero2)}");
-                }
-            Console.ReadKey();
+        //        if (operador == "+")
+        //        {
+        //            Console.WriteLine($"Resultado: {Somatoria(numero1, numero2)}");
+        //        }
+        //        else if (operador == "-")
+        //        {
+        //            Console.WriteLine($"Resultado: {Subtracao(numero1, numero2)}");
+        //        }
+        //        else if (operador == "*")
+        //        {
+        //            Console.WriteLine($"Resultado: {Multiplicacao(numero1, numero2)}");
+        //        }
+        //        if (operador == "/")
+        //        {
+        //            Console.WriteLine($"Resultado: {Divisao(numero1, numero2)}");
+        //        }
+        //    Console.ReadKey();
 
-            int valor1 = 100;
-            int valor2 = 200;
-            Console.WriteLine($"Nova somatória: { Somatoria(valor1, valor2) } ");
+        //    int valor1 = 100;
+        //    int valor2 = 200;
+        //    Console.WriteLine($"Nova somatória: { Somatoria(valor1, valor2) } ");
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
 
-
-
-        public static int Somatoria(int numero1, int numero2)
-        {
-            return numero1 + numero2;
-        }
-        public static int Subtracao(int numero1, int numero2)
-        {
-            return numero1 - numero2;
-        }
-        public static int Multiplicacao(int numero1, int numero2)
-        {
-            return numero1 * numero2;
-        }
-        public static int Divisao(int numero1, int numero2)
-        {
-            return numero1 / numero2;
-        }
+        //public static int Somatoria(int numero1, int numero2)
+        //{
+        //    return numero1 + numero2;
+        //}
+        //public static int Subtracao(int numero1, int numero2)
+        //{
+        //    return numero1 - numero2;
+        //}
+        //public static int Multiplicacao(int numero1, int numero2)
+        //{
+        //    return numero1 * numero2;
+        //}
+        //public static int Divisao(int numero1, int numero2)
+        //{
+        //    return numero1 / numero2;
+        //}
     }
 }
