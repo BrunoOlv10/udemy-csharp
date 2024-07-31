d7f925a7e65f06c41285c0c4645b5f1c43ba11a8
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-30 23:08:50 -0300

    recuperar exercicio

--- projeto-teste/try-catch.cs
+++ projeto-teste/try-catch.cs
@@ -8,36 +8,36 @@ namespace curso_udemy
 {
     internal class try_catch
     {
-        static void Main(string[] args)
-        {
-            string[] nomes = new string[] { "bruno", "vilma", "pablo", "joão", "carlos", 
-                                            "pedro" };
+        //static void Main(string[] args)
+        //{
+        //    string[] nomes = new string[] { "bruno", "vilma", "pablo", "joão", "carlos",
+        //                                    "pedro" };
 
-            try
-            {
-                Console.WriteLine();
-                Console.Write("Buscar um nome: ");
-                string nomeBuscar = Console.ReadLine();
+        //    try
+        //    {
+        //        Console.WriteLine();
+        //        Console.Write("Buscar um nome: ");
+        //        string nomeBuscar = Console.ReadLine();
 
-                int posicao = Array.IndexOf(nomes, nomeBuscar.ToLower());
+        //        int posicao = Array.IndexOf(nomes, nomeBuscar.ToLower());
 
-                Console.WriteLine($"Achei o nome: {nomes[posicao]} na posição: {posicao}");
+        //        Console.WriteLine($"Achei o nome: {nomes[posicao]} na posição: {posicao}");
 
-                int valor = 0;
-                int res = 10 / valor;
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Não é possível fazer divisão por zero");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("O nome não foi encontrado");
-            }
-            finally
-            {
-                Console.ReadKey();
-            }
-        }
+        //        int valor = 0;
+        //        int res = 10 / valor;
+        //    }
+        //    catch (DivideByZeroException ex)
+        //    {
+        //        Console.WriteLine("Não é possível fazer divisão por zero");
+        //    }
+        //    catch (Exception)
+        //    {
+        //        Console.WriteLine("O nome não foi encontrado");
+        //    }
+        //    finally
+        //    {
+        //        Console.ReadKey();
+        //    }
+        //}
     }
 }
