471399de69a1d525c2c8b77c2115fbf40abbae3a
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-30 23:38:16 -0300

    add exercicio algoritmo ordenacao

---
+++ projeto-teste/algoritmo-ordenacao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class algoritmo_ordenacao
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 100, 99, 55, 30, 90 };
+            int aux = 0;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                for(int j = 0; j < vetor.Length; j++)
+                {
+                    if (vetor[i] < vetor[j])
+                    {
+                        aux = vetor[i];
+
+                        vetor[i] = vetor[j];
+
+                        vetor[j] = aux;
+                    }
+                }
+            }
+            foreach (var item in vetor)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Resultado");
+            Console.ReadKey();
+        }
+    }
+}
