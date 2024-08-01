818f92f1eaacc41b0c5467952c91e2400b4fb03f
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-01 15:55:55 -0300

    add exercicio retangulos de numeros

--- projeto-teste/exercicio2-multiplicao.cs
+++ projeto-teste/exercicio2-multiplicao.cs
@@ -8,23 +8,23 @@ namespace curso_udemy
 {
     class exercicio_multiplicao
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Digite o primeiro número: ");
-            int numero1 = Convert.ToInt16(Console.ReadLine());
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Digite o primeiro número: ");
+        //    int numero1 = Convert.ToInt16(Console.ReadLine());
 
-            Console.Write("Digite o segundo número: ");
-            int numero2 = Convert.ToInt16(Console.ReadLine());
+        //    Console.Write("Digite o segundo número: ");
+        //    int numero2 = Convert.ToInt16(Console.ReadLine());
 
-            Console.Write("Digite o terceiro número: ");
-            int numero3 = Convert.ToInt16(Console.ReadLine());
+        //    Console.Write("Digite o terceiro número: ");
+        //    int numero3 = Convert.ToInt16(Console.ReadLine());
 
-            var multiplicacao = numero1 * numero2 * numero3;
+        //    var multiplicacao = numero1 * numero2 * numero3;
 
-            Console.WriteLine();
-            Console.Write($"O resultado da multiplicação entre {numero1}, {numero2} e {numero3} é: {multiplicacao}");
+        //    Console.WriteLine();
+        //    Console.Write($"O resultado da multiplicação entre {numero1}, {numero2} e {numero3} é: {multiplicacao}");
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
