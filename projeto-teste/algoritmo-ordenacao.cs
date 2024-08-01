42d552a34678ec36e92f75ff53cbd752e77b0df5
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-07-31 21:30:03 -0300

    add exercicio busca binaria

--- projeto-teste/algoritmo-ordenacao.cs
+++ projeto-teste/algoritmo-ordenacao.cs
@@ -8,31 +8,31 @@ namespace curso_udemy
 {
     class algoritmo_ordenacao
     {
-        static void Main(string[] args)
-        {
-            int[] vetor = { 100, 99, 55, 30, 90 };
-            int aux = 0;
+        //static void Main(string[] args)
+        //{
+        //    int[] vetor = { 100, 99, 55, 30, 90 };
+        //    int aux = 0;
 
-            for (int i = 0; i < vetor.Length; i++)
-            {
-                for(int j = 0; j < vetor.Length; j++)
-                {
-                    if (vetor[i] < vetor[j])
-                    {
-                        aux = vetor[i];
+        //    for (int i = 0; i < vetor.Length; i++)
+        //    {
+        //        for(int j = 0; j < vetor.Length; j++)
+        //        {
+        //            if (vetor[i] < vetor[j])
+        //            {
+        //                aux = vetor[i];
 
-                        vetor[i] = vetor[j];
+        //                vetor[i] = vetor[j];
 
-                        vetor[j] = aux;
-                    }
-                }
-            }
-            foreach (var item in vetor)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("Resultado");
-            Console.ReadKey();
-        }
+        //                vetor[j] = aux;
+        //            }
+        //        }
+        //    }
+        //    foreach (var item in vetor)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //    Console.WriteLine("Resultado");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ projeto-teste/busca-binaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class busca_binaria
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+            Console.Write("Insira um valor a buscar: ");
+            int valorBuscado = Convert.ToInt16(Console.ReadLine());
+
+            int inf, meio, sup;
+            inf = -1;
+            sup = vetor.Length;
+
+            while (inf < sup - 1)
+            {
+                meio = (inf + sup) / 2;
+                if (vetor[meio] < valorBuscado)
+                    inf = meio;
+                else
+                    sup = meio;
+            }
+
+            if (sup >= 0 && sup < vetor.Length)
+            {
+                if (vetor[sup] == valorBuscado)
+                    Console.WriteLine("Valor encontrado na posição: " + sup);
+                else
+                    Console.WriteLine("Valor não foi encontrado");
+            }
+            else
+                Console.WriteLine("Valor não foi encontrado");
+
+            Console.ReadKey();
+        }
+    }
+}
