b1f27123be8e13bc627b6f2f8b5e47045dece741
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-01 15:35:12 -0300

    add exercicio trocar numeros de variaveis

--- projeto-teste/algoritmo-corte-controle.cs
+++ projeto-teste/algoritmo-corte-controle.cs
@@ -5,76 +5,76 @@ namespace curso_udemy
 {
     class algoritmo_corte_controle
     {
-        static void Main(string[] args)
-        {
-            string[,] dadosProdutos = new string[,] {
-                { "1", "martelos", "01/2019", "10" },
-                { "1", "martelos", "01/2019", "14" },
-                { "1", "martelos", "02/2019", "5" },
-                { "1", "martelos", "03/2019", "12" },
-                { "2", "chaves fenda", "02/2019", "2" },
-                { "2", "chaves fenda", "03/2019", "1" },
-                { "3", "chaves philips", "01/2019", "20" },
-                { "3", "chaves philips", "01/2019", "30" },
-                { "3", "chaves philips", "01/2019", "50" },
-                { "4", "baldes", "03/2019", "100" },
-                { "5", "pas", "02/2019", "50" },
-                { "5", "pas", "03/2019", "150" },
-            };
+        //static void Main(string[] args)
+        //{
+        //    string[,] dadosProdutos = new string[,] {
+        //        { "1", "martelos", "01/2019", "10" },
+        //        { "1", "martelos", "01/2019", "14" },
+        //        { "1", "martelos", "02/2019", "5" },
+        //        { "1", "martelos", "03/2019", "12" },
+        //        { "2", "chaves fenda", "02/2019", "2" },
+        //        { "2", "chaves fenda", "03/2019", "1" },
+        //        { "3", "chaves philips", "01/2019", "20" },
+        //        { "3", "chaves philips", "01/2019", "30" },
+        //        { "3", "chaves philips", "01/2019", "50" },
+        //        { "4", "baldes", "03/2019", "100" },
+        //        { "5", "pas", "02/2019", "50" },
+        //        { "5", "pas", "03/2019", "150" },
+        //    };
 
-            int totalProduto = 0;
-            int totalGeral = 0;
-            string idProdutoAtual = "";
-            string idProdutoAnterior = "";
-            int linha = 0;
-            int numLinhas = dadosProdutos.GetLength(0);
+        //    int totalProduto = 0;
+        //    int totalGeral = 0;
+        //    string idProdutoAtual = "";
+        //    string idProdutoAnterior = "";
+        //    int linha = 0;
+        //    int numLinhas = dadosProdutos.GetLength(0);
 
-            Dictionary<string, int> vendasPorData = new Dictionary<string, int>();
+        //    Dictionary<string, int> vendasPorData = new Dictionary<string, int>();
 
-            Console.WriteLine("Id - Produto - Data - Qnd");
-            while (linha < numLinhas)
-            {
-                idProdutoAtual = dadosProdutos[linha, 0];
-                idProdutoAnterior = idProdutoAtual;
+        //    Console.WriteLine("Id - Produto - Data - Qnd");
+        //    while (linha < numLinhas)
+        //    {
+        //        idProdutoAtual = dadosProdutos[linha, 0];
+        //        idProdutoAnterior = idProdutoAtual;
 
-                totalProduto = 0;
+        //        totalProduto = 0;
 
-                while (linha < numLinhas && dadosProdutos[linha, 0] == idProdutoAtual)
-                {
-                    string dataVendaAtual = dadosProdutos[linha, 2];
+        //        while (linha < numLinhas && dadosProdutos[linha, 0] == idProdutoAtual)
+        //        {
+        //            string dataVendaAtual = dadosProdutos[linha, 2];
 
-                    Console.WriteLine($"{dadosProdutos[linha, 0]} - {dadosProdutos[linha, 1]} - {dadosProdutos[linha, 2]} - {dadosProdutos[linha, 3]}");
+        //            Console.WriteLine($"{dadosProdutos[linha, 0]} - {dadosProdutos[linha, 1]} - {dadosProdutos[linha, 2]} - {dadosProdutos[linha, 3]}");
 
-                    if (int.TryParse(dadosProdutos[linha, 3], out int quantidadeProdutos))
-                    {
-                        totalProduto += quantidadeProdutos;
+        //            if (int.TryParse(dadosProdutos[linha, 3], out int quantidadeProdutos))
+        //            {
+        //                totalProduto += quantidadeProdutos;
 
-                        if (vendasPorData.ContainsKey(dataVendaAtual))
-                            vendasPorData[dataVendaAtual] += quantidadeProdutos;
+        //                if (vendasPorData.ContainsKey(dataVendaAtual))
+        //                    vendasPorData[dataVendaAtual] += quantidadeProdutos;
 
-                        else
-                            vendasPorData[dataVendaAtual] = quantidadeProdutos;
-                    }
+        //                else
+        //                    vendasPorData[dataVendaAtual] = quantidadeProdutos;
+        //            }
 
-                    linha++;
-                }
+        //            linha++;
+        //        }
 
-                Console.WriteLine($"Total do Produto: {totalProduto}");
-                Console.WriteLine();
-                totalGeral += totalProduto;
-            }
+        //        Console.WriteLine($"Total do Produto: {totalProduto}");
+        //        Console.WriteLine();
+        //        totalGeral += totalProduto;
+        //    }
 
-            Console.WriteLine($"Total Geral: {totalGeral}");
-            Console.WriteLine();
+        //    Console.WriteLine($"Total Geral: {totalGeral}");
+        //    Console.WriteLine();
 
-            Console.WriteLine("Total de vendas por data:");
-            foreach (var data in vendasPorData)
-            {
-                Console.WriteLine($"{data.Key}: {data.Value}");
-            }
+        //    Console.WriteLine("Total de vendas por data:");
+        //    foreach (var data in vendasPorData)
+        //    {
+        //        Console.WriteLine($"{data.Key}: {data.Value}");
+        //    }
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
 
---
+++ projeto-teste/trocar-numeros-entre-variaveis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class trocar_numeros_entre_variaveis
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int numero2 = Convert.ToInt16(Console.ReadLine());
+
+            var aux = 0;
+            aux = numero1;
+            numero1 = numero2;
+            numero2 = aux;
+
+            Console.WriteLine();
+            Console.WriteLine($"Número 1: {numero1}");
+            Console.WriteLine($"Número 2: {numero2}");
+            Console.ReadKey();
+        }
+    }
+}
