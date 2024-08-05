e927e6bf40f616f424b7fdc6811627eb52251af9
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-04 22:33:24 -0300

    add ex media

---
+++ projeto-teste/exercicio3-media.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class exercicio3_media
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Digite o primeiro número: ");
+        //    var numero1 = Convert.ToInt16(Console.ReadLine());
+
+        //    Console.Write("Digite o segundo número: ");
+        //    var numero2 = Convert.ToInt16(Console.ReadLine());
+
+        //    Console.Write("Digite o terceiro número: ");
+        //    var numero3 = Convert.ToInt16(Console.ReadLine());
+
+        //    Console.Write("Digite o quarto número: ");
+        //    var numero4 = Convert.ToInt16(Console.ReadLine());
+
+        //    var media = (numero1 + numero2 + numero3 + numero4) / 4;
+
+        //    Console.WriteLine($"A média entre os números {numero1}, {numero2}, {numero3}, {numero4} é {media}");
+        //    Console.ReadKey();
+        //}
+    }
+}
