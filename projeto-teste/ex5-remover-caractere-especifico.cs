c3543bfafa5e7569e526ed34297159c25b461ef5
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-05 15:37:50 -0300

    lendo excel

--- projeto-teste/ex5-remover-caractere-especifico.cs
+++ projeto-teste/ex5-remover-caractere-especifico.cs
@@ -8,25 +8,25 @@ namespace curso_udemy
 {
     class remover_caractere_especifico
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Escreva uma palavra: ");
-            string palavra = Console.ReadLine();
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Escreva uma palavra: ");
+        //    string palavra = Console.ReadLine();
 
-            Console.Write("Escolha a posição da letra a ser removida: ");
-            int posicao = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Escolha a posição da letra a ser removida: ");
+        //    int posicao = Convert.ToInt32(Console.ReadLine());
 
-            string semCaractere = null;
+        //    string semCaractere = null;
 
-            if (posicao < palavra.Length)
-            {
-                semCaractere = palavra.Remove(posicao);
-                Console.Write($"Letra da posição {posicao} removida da palavra {palavra}: {semCaractere}");
-            }
-            else
-                Console.WriteLine("Posição inexistente na palavra");
+        //    if (posicao < palavra.Length)
+        //    {
+        //        semCaractere = palavra.Remove(posicao);
+        //        Console.Write($"Letra da posição {posicao} removida da palavra {palavra}: {semCaractere}");
+        //    }
+        //    else
+        //        Console.WriteLine("Posição inexistente na palavra");
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
