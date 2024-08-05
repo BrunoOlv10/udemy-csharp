5797082c3d4221b781c96fd97e4f99ab966a095e
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-04 22:49:32 -0300

    add ex remover caractere especifico

---
+++ projeto-teste/ex5-remover-caractere-especifico.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class remover_caractere_especifico
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva uma palavra: ");
+            string palavra = Console.ReadLine();
+
+            Console.Write("Escolha a posição da letra a ser removida: ");
+            int posicao = Convert.ToInt32(Console.ReadLine());
+
+            string semCaractere = null;
+
+            if (posicao < palavra.Length)
+            {
+                semCaractere = palavra.Remove(posicao);
+                Console.Write($"Letra da posição {posicao} removida da palavra {palavra}: {semCaractere}");
+            }
+            else
+                Console.WriteLine("Posição inexistente na palavra");
+
+            Console.ReadKey();
+        }
+    }
+}
