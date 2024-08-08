5e89b79adaa821d8eccafae3b0e06bcaefdc1d4b
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-08 20:30:56 -0300

    add ex soma 1000 primeiros numeros primos

--- projeto-teste/ex8-palavra-longa.cs
+++ projeto-teste/ex8-palavra-longa.cs
@@ -8,22 +8,22 @@ namespace curso_udemy
 {
     class ex8_palavra_longa
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Escreva uma frase: ");
-            string[] palavra = Console.ReadLine().Split(' ');
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Escreva uma frase: ");
+        //    string[] palavra = Console.ReadLine().Split(' ');
 
-            string maiorPalavra = "";
+        //    string maiorPalavra = "";
 
-            for (int i = 0; i < palavra.Length; i++)
-            {
-                var palavraAtual = palavra[i];
-                if (palavraAtual.Length > maiorPalavra.Length)
-                    maiorPalavra = palavraAtual;
-            }
+        //    for (int i = 0; i < palavra.Length; i++)
+        //    {
+        //        var palavraAtual = palavra[i];
+        //        if (palavraAtual.Length > maiorPalavra.Length)
+        //            maiorPalavra = palavraAtual;
+        //    }
 
-            Console.WriteLine($"A palavra mais longa da frase é: {maiorPalavra}");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine($"A palavra mais longa da frase é: {maiorPalavra}");
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ projeto-teste/ex9-soma-numeros-primos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex9_soma_numeros_primos
+    {
+        static void Main(string[] args)
+        {
+            int somaPrimos = 0;
+            int encontrados = 0;
+            int numeroAtual = 2;
+
+            while (encontrados < 1000)
+            {
+                int numerosDivisiveis = 0;
+
+                for (int j = 1; j <= numeroAtual; j++)
+                {
+                    if (numeroAtual % j == 0)
+                    {
+                        numerosDivisiveis += 1;
+                    }
+                }
+
+                if (numerosDivisiveis == 2)
+                {
+                    encontrados += 1;
+                    somaPrimos += numeroAtual;
+                }
+
+                numeroAtual++;
+                
+            }
+            Console.WriteLine($"A soma dos primeiros 1000 números primos é: {somaPrimos}");
+            Console.ReadKey();
+        }
+    }
+}
