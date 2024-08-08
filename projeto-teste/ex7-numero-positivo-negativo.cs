32687128f0429a52d61bfdca1063e2b8e20ebe30
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-07 21:29:23 -0300

    add ex numero positivo e negativo

---
+++ projeto-teste/ex7-numero-positivo-negativo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex7_numero_positivo_negativo
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int numero2 = Convert.ToInt16(Console.ReadLine());
+
+            if (numero1 > 0 && numero2 < 0 || numero1 < 0 && numero2 > 0)
+                Console.WriteLine(true);
+            else
+                Console.WriteLine(false);
+
+            Console.ReadKey();
+        }
+    }
+}
