dc3645e9d05fcce960fa33a54a73cfcb33637af9
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-09 16:14:49 -0300

    add ex maiuscula e minuscula

--- projeto-teste/ex12-valor-mais-proximo.cs
+++ projeto-teste/ex12-valor-mais-proximo.cs
@@ -8,33 +8,33 @@ namespace curso_udemy
 {
     class ex12_valor_mais_proximo
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Digite o primeiro número: ");
-            int numero1 = Convert.ToInt16(Console.ReadLine());
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Digite o primeiro número: ");
+        //    int numero1 = Convert.ToInt16(Console.ReadLine());
 
-            Console.Write("Digite o segundo número: ");
-            int numero2 = Convert.ToInt16(Console.ReadLine());
+        //    Console.Write("Digite o segundo número: ");
+        //    int numero2 = Convert.ToInt16(Console.ReadLine());
 
-            int referencia = 20;
+        //    int referencia = 20;
 
-            int diferenca1 = referencia - numero1; 
-            int diferenca2 = referencia - numero2;
+        //    int diferenca1 = referencia - numero1; 
+        //    int diferenca2 = referencia - numero2;
 
-            int menorDiferenca = 0;
-            if (diferenca1 < diferenca2)
-            {
-                menorDiferenca = numero1;
-            }
+        //    int menorDiferenca = 0;
+        //    if (diferenca1 < diferenca2)
+        //    {
+        //        menorDiferenca = numero1;
+        //    }
             
-            if (diferenca2 < diferenca1)
-            {
-                menorDiferenca = numero2;
-            }
+        //    if (diferenca2 < diferenca1)
+        //    {
+        //        menorDiferenca = numero2;
+        //    }
 
-            Console.WriteLine($"O número mais próximo de 20 é {menorDiferenca}");
+        //    Console.WriteLine($"O número mais próximo de 20 é {menorDiferenca}");
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
---
+++ projeto-teste/ex13-maiuscula-minuscula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex13_maiuscula_minuscula
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva uma palavra (se tiver menos de 4 caracteres vai ficar inteira em MAIÚSCULA): ");
+            string palavra = Console.ReadLine();
+
+            if (palavra.Length < 4)
+                palavra = palavra.ToUpper();
+
+            else
+                palavra = palavra.ToLower();
+
+            Console.Write($"A palavra ficou assim: {palavra}");
+
+            Console.ReadKey();
+        }
+    }
+}
