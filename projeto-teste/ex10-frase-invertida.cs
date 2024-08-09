695b16e0df78cd69cc38819dd7d7094f2550feab
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-09 15:46:07 -0300

    add ex contem palavra

---
+++ projeto-teste/ex10-frase-invertida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex10_frase_revertida
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Escreva uma frase: ");
+        //    string[] fraseOriginal = Console.ReadLine().Split(' ');
+
+        //    string fraseRevertida = string.Join(" ", fraseOriginal.Reverse());
+
+        //    Console.WriteLine($"A frase ficou assim: {fraseRevertida}");
+
+        //    Console.ReadKey();
+        //}
+    }
+}
