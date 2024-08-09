dd78355da8f0afdcd049dfb78ac240e3f0a4776e
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-09 16:04:48 -0300

    add ex valor mais proximo

---
+++ projeto-teste/ex11-contem-palavra.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Office2010.PowerPoint;
+
+namespace curso_udemy
+{
+    class ex11_comeca_palavra_especifica
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Escreva uma frase: ");
+        //    string[] frase = Console.ReadLine().Split(' ');
+
+        //    Console.Write("Escreva uma palavra para ver se a mesma contém na frase anterior: ");
+        //    string palavra = Console.ReadLine();
+
+        //    bool palavraEncontrada = false;
+
+        //    foreach (var pedaco in frase)
+        //    {
+        //        if (pedaco == palavra)
+        //        {
+        //            palavraEncontrada = true;
+        //            break;
+        //        }
+
+        //    }
+
+        //    string fraseOriginal = string.Join(" ", frase);
+
+        //    if (palavraEncontrada)
+        //        Console.WriteLine($"A palavra '{palavra}' está presente na frase: {fraseOriginal}");
+            
+        //    else
+        //        Console.WriteLine($"A palavra '{palavra}' NÃO está presente na frase: {fraseOriginal}");
+
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ projeto-teste/ex12-valor-mais-proximo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex12_valor_mais_proximo
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            int numero2 = Convert.ToInt16(Console.ReadLine());
+
+            int referencia = 20;
+
+            int diferenca1 = referencia - numero1; 
+            int diferenca2 = referencia - numero2;
+
+            int menorDiferenca = 0;
+            if (diferenca1 < diferenca2)
+            {
+                menorDiferenca = numero1;
+            }
+            
+            if (diferenca2 < diferenca1)
+            {
+                menorDiferenca = numero2;
+            }
+
+            Console.WriteLine($"O número mais próximo de 20 é {menorDiferenca}");
+
+            Console.ReadKey();
+        }
+    }
+}
