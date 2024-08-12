9d97fe89d52e41ef59fc41580ab1b898dbe7466e
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-11 21:54:41 -0300

    add ex girar matriz esquerda

---
+++ projeto-teste/ex16-girar-matriz-esquerda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy
+{
+    class ex16_girar_matriz_esquerda
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva números para entrar na matriz (separados por vírgula): ");
+            string entrada = Console.ReadLine();
+
+            string[] numerosString = entrada.Split(',').Select(n => n.Trim()).ToArray(); ;
+
+            int[] numerosSeparados = numerosString.Select(int.Parse).ToArray();
+
+            int[] numerosRotacionado = numerosSeparados.Skip(1).Concat(numerosSeparados.Take(1)).ToArray();
+
+            Console.WriteLine("Vetor rotacionado: [" + string.Join(", ", numerosRotacionado) + "]");
+            Console.ReadKey();
+        }
+    }
+}
