53342c664cb7208ce9cc09075d46d77fbbcf15ba
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-08-12 09:14:14 -0300

    add ex palindromo

---
+++ projeto-teste/ex18-palindromo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy.recuperar_instituicao_ano
+{
+    public class ex18_polindromo
+    {
+        public static string ObterPalavra()
+        {
+            Console.Write("Escreva uma palavra: ");
+            return Console.ReadLine();
+        }
+
+        public static bool Palindromo(string palavra)
+        {
+            string reverso = new string(palavra.Reverse().ToArray());
+            return palavra.Equals(reverso, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static void ExibirResultado(string palavra, bool palindromo)
+        {
+            if (palindromo)
+            {
+                Console.WriteLine($"A palavra \"{palavra}\" é um palíndromo.");
+            }
+            else
+            {
+                Console.WriteLine($"A palavra \"{palavra}\" NÃO é um palíndromo.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ projeto-teste/testar-exs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using curso_udemy.recuperar_instituicao_ano;
+
+namespace curso_udemy
+{
+    public class testar_exs
+    {
+        static void Main(string[] args)
+        {
+            string palavra = ex18_polindromo.ObterPalavra();
+
+            bool palindromo = ex18_polindromo.Palindromo(palavra);
+
+            ex18_polindromo.ExibirResultado(palavra, palindromo);
+        }
+    }
+}
