b3ef5a06c3ca06fbb46d53fbe7f77c92fa9af063
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-08 15:11:57 -0300

    add classes e atributos

---
+++ projeto-teste/classes/PessoaJuridica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy.classes
+{
+    public class PessoaJuridica
+    {
+        public string CNPJ { get; set; }
+    }
+}
---
+++ projeto-teste/classes/classesAbstratas/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_udemy.classes.classesAbstratas
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+    }
+}
