a3dd31a2cc6d4ce4c1ce5733cbde0a44461ebb54
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-12 21:34:11 -0300

    add ex encapsulamento

--- projeto-teste/classes/Carro.cs
+++ projeto-teste/classes/Carro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,19 @@ namespace projeto.classes
 {
     public class Carro
     {
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
+        public string Marca { get; private set; }
+        public string Modelo { get; private set; }
         public string Cor { get; set; }
         public bool EstaAlugado { get; set; }
         public double PrecoAluguel { get; set; }
+        public int Senha { get; set; }
 
-        public Carro(string Marca, string Modelo)
+        public Carro(string Marca, string Modelo, double PrecoAluguel, int Senha)
         {
             this.Marca = Marca;
             this.Modelo = Modelo;
+            this.PrecoAluguel = PrecoAluguel;
+            this.Senha = Senha;
         }
 
         public void Ligar()
@@ -45,6 +49,17 @@ namespace projeto.classes
         {
             EstaAlugado = false;
             Console.WriteLine($"O carro {Marca} foi devolvido");
+        }
+
+        public void MudarPrecoAluguel(int senha, double novoPrecoAluguel)
+        {
+            if (this.Senha == senha)
+            {
+                this.PrecoAluguel = novoPrecoAluguel;
+                Console.WriteLine("Preço do aluguel alterado com sucesso!");
+            }
+            else
+                Console.WriteLine($"Senha incorreta! Não autorizado a mudança de preço do aluguel do carro {Marca} {Modelo}");
         }
     }
 }
--- projeto-teste/classes/CriacaoCarro.cs
+++ projeto-teste/classes/CriacaoCarro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using projeto.classes;
 
 namespace projeto
@@ -11,25 +12,33 @@ namespace projeto
     {
         public static void CriarCarro()
         {
-            Carro carro = new Carro("Nissan", "GTR")
+            Carro carro = new Carro("Nissan", "GTR", 1000, 1234)
             {
                 //Marca = "VW",
+                //Cor = "Azul",
                 //Modelo = "1969",
-                Cor = "Azul",
-                PrecoAluguel = 3000
+                //PrecoAluguel = 3000
             };
 
-            Console.WriteLine($"Meu primeiro carro da frota é da marca: {carro.Marca} e do modelo {carro.Modelo}");
+            Console.Write("Insira o novo preço do aluguel: ");
+            var preco = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Quer alugar esse carro (coloque 'sim' ou 'não'): ");
-            string alugar = Console.ReadLine().ToLower();
+            Console.Write("Insira a senha cadastrada (apenas números): ");
+            int senha = Convert.ToInt32(Console.ReadLine());
 
-            if (alugar == "sim")
-                carro.Alugar();
-            else
-                Console.WriteLine("O carro não foi alugado");
+            carro.MudarPrecoAluguel(senha, preco);
 
-            Console.WriteLine($"O {carro.Marca} está alugado? {carro.EstadoAluguel()}");
+            Console.WriteLine($"Meu primeiro carro da frota é da marca: {carro.Marca} e do modelo {carro.Modelo} e com preço de aluguel { carro.PrecoAluguel }");
+
+            //Console.Write("Quer alugar esse carro (coloque 'sim' ou 'não'): ");
+            //string alugar = Console.ReadLine().ToLower();
+
+            //if (alugar == "sim")
+            //    carro.Alugar();
+            //else
+            //    Console.WriteLine("O carro não foi alugado");
+
+            //Console.WriteLine($"O {carro.Marca} está alugado? {carro.EstadoAluguel()}");
 
             Console.ReadKey();
         }
