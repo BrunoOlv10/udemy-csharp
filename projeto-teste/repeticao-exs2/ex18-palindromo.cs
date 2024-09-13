e41a05667bc6886973c02469c41658ed0a3afa5e
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-13 16:42:52 -0300

    add ex identificar se tem algum palindromo

---
+++ projeto-teste/repeticao-exs2/ex18-palindromo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs2
+{
+    public class ex18_palindromo
+    {
+        public static void IdentificarPalindromos()
+        {
+            Console.Write("Escreva uma sequência de caracteres, palavras, números, símbolos (separados por vírgulas): ");
+            string[] elementos = Console.ReadLine().Split(',').Select(e => e.Trim()).ToArray();
+
+            bool encontrouPalindromos = false;
+
+            foreach (string elemento in elementos)
+            {
+                if (string.IsNullOrEmpty(elemento))
+                {
+                    continue;
+                }
+
+                string invertido = new string(elemento.Reverse().ToArray());
+
+                if (elemento.Equals(invertido, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!encontrouPalindromos)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("O(s) palíndromo(s) são: ");
+                        encontrouPalindromos = true;
+                    }
+                    Console.WriteLine($"{elemento} - {invertido}");
+                }
+            }
+
+            if (!encontrouPalindromos)
+            {
+                Console.WriteLine("Nenhum palíndromo encontrado.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- projeto-teste/testar-exs.cs
+++ projeto-teste/testar-exs.cs
@@ -13,7 +13,7 @@ namespace projeto
     {
         static void Main(string[] args)
         {
-            repeticao_exs2.ex17_maior_valor_array.IdentificarMaiorValorArray();
+            repeticao_exs2.ex18_palindromo.IdentificarPalindromos();
         }
     }
 }
