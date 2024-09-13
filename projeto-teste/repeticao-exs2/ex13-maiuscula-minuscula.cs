0b84eb9f227e5c84cc0fc3b50d9c28882cd03425
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-13 12:01:50 -0300

    add ex identificar palavra com 4 letras ou menos e colocar inteira em maiuscula

---
+++ projeto-teste/repeticao-exs2/ex13-maiuscula-minuscula.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs2
+{
+    public class ex13_maiuscula_minuscula
+    {
+        public static void IdentificarMaiuscula()
+        {
+            try
+            {
+                Console.WriteLine("Regra: se a palavra conter 4 ou menos letras, ela será exibida toda em maiúscula, se não será " +
+                    "exibida em minúcula");
+                Console.Write("Digite uma palavra (apenas letras): ");
+                string palavra = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(palavra) || !palavra.All(c => char.IsLetter(c) || char.IsWhiteSpace(c)))
+                    throw new Exception();
+
+                string palavraAlterada = "";
+                if (palavra.Length <= 4)
+                    palavraAlterada = palavra.ToUpper();
+
+                else
+                    palavraAlterada = palavra.ToLower();
+
+                Console.WriteLine();
+                Console.WriteLine($"Seguindo a regra, a palavra inserida foi '{palavra}' e ela ficou dessa forma: '{palavraAlterada}'");
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Digite apenas palavras e letras!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- projeto-teste/testar-exs.cs
+++ projeto-teste/testar-exs.cs
@@ -13,7 +13,7 @@ namespace projeto
     {
         static void Main(string[] args)
         {
-            repeticao_exs2.ex12_valor_mais_proximo.IdentificarValorProximo();
+            repeticao_exs2.ex13_maiuscula_minuscula.IdentificarMaiuscula();
         }
     }
 }
