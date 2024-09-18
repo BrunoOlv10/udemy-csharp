ff2f4af1d5d41d429b0868f2edf1c96daf9a5a4a
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-18 10:06:19 -0300

    add ex piramide formada por caracteres

---
+++ projeto-teste/repeticao-exs2/ex22-piramide-caracteres.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs2
+{
+    public class ex22_piramide_caracteres
+    {
+        public static void MontarPiramide()
+        {
+            try
+            {
+                Console.Write("Insira o elemento que formará a pirâmide: ");
+                string elemento = Console.ReadLine();
+
+                if (elemento.Length < 1 || elemento.Length > 3)
+                    throw new Exception();
+
+                Console.WriteLine();
+
+                Console.Write("Insira a altura da pirâmide: ");
+                int altura = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i <= altura; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(elemento);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Insira apenas números para a altura!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Insira até três elementos para formar a pirâmide!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
