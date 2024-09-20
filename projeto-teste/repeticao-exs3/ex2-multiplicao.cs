e7246d48924cb95e747482f76c9370be6bbaf706
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-09-20 10:59:38 -0300

    add ex multiplicacao

---
+++ projeto-teste/repeticao-exs3/ex2-multiplicao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs3
+{
+    public class ex2_multiplicao
+    {
+        public static void Multiplicar()
+        {
+            try
+            {
+                int[] numeros = new int[2];
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.Write($"Insira o número {i + 1}: ");
+                    string entrada = Console.ReadLine();
+                    numeros[i] = int.Parse(entrada);
+                }
+
+                int multiplicacao = numeros[0] * numeros[1];
+
+                Console.WriteLine();
+                Console.WriteLine($"A multiplicação entre os números {numeros[0]} e {numeros[1]} resulta em {multiplicacao}");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Insira apenas números!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- projeto-teste/testar-exs.cs
+++ projeto-teste/testar-exs.cs
@@ -13,7 +13,7 @@ namespace projeto
     {
         static void Main(string[] args)
         {
-            repeticao_exs3.ex1_trocar_numeros_variaveis.TrocarNumerosVariaveis();
+            repeticao_exs3.ex2_multiplicao.Multiplicar();
         }
     }
 }
