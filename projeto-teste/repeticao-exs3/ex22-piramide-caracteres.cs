960ca35345751b37154d376fd7adc35842d1a87b
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-11-04 09:20:28 -0300

    add ex pirâmide de caracteres

---
+++ projeto-teste/repeticao-exs3/ex22-piramide-caracteres.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs3
+{
+    public class ex22_piramide_caracteres
+    {
+        public static void GerarPiramide()
+        {
+            try
+            {
+                Console.Write("Insira o caractere ou a sequência de caracteres que formará a pirâmide: ");
+                string caractere = Console.ReadLine();
+
+                Console.Write("Digite a altura que terá a pirâmide: ");
+                int altura = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                for (int i = 0; i < altura; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write(caractere);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Digite apenas valores numéricos e inteiros para a altura da pirâmide");
+                Console.ReadKey();
+            }
+        }
+    }
+}
