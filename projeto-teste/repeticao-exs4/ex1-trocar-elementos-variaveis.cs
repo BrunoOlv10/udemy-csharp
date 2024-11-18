16179ec32d4fa9e5d6337a22b62cc3402053cdfa
Bruno Oliveira <bruno.olvslv@gmail.com>
2024-11-18 10:39:38 -0300

    add ex trocar elementos variável

---
+++ projeto-teste/repeticao-exs4/ex1-trocar-elementos-variaveis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs4
+{
+    public class ex1_trocar_elementos_variaveis
+    {
+        public static void TrocarElementosVariaveis()
+        {
+            Console.WriteLine("Insira dois elementos abaixo");
+            string elemento1 = "", elemento2 = "";
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"Insira o elemento {i + 1}: ");
+                string entrada = Console.ReadLine();
+
+                if (i == 0)
+                    elemento1 = entrada;
+
+                if (i == 1)
+                    elemento2 = entrada;
+            }
+
+            var aux = elemento1;
+            string elementoTrocado1 = elemento2;
+            string elementoTrocado2 = aux;
+
+            Console.WriteLine();
+            Console.WriteLine($"Os elementos inseridos foram '{elemento1}' e '{elemento2}', após serem trocados de variável ficou '{elementoTrocado1}' e '{elementoTrocado2}'");
+            Console.ReadKey();
+        }
+    }
+}
--- projeto-teste/testar-exs.cs
+++ projeto-teste/testar-exs.cs
@@ -20,7 +20,7 @@ namespace projeto
             //VendaEmpresa venda = new VendaEmpresa();
             //Menu_Principal.SelecionarMenu(funcionario, produto, venda);
 
-            repeticao_exs3.ex32_ordenar_strings_bubble_sort.OrdenarBubbleSort();
+            repeticao_exs4.ex1_trocar_elementos_variaveis.TrocarElementosVariaveis();
         }
     }
 }
