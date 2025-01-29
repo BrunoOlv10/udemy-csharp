9c8c790630fd7068e0c63513c7b43254929808b6
Bruno Oliveira <bruno.olvslv@gmail.com>
2025-01-29 16:23:08 -0300

    add ex ordenar inteiros bubble sort

---
+++ projeto-teste/repeticao-exs4/ex32-ordenar-inteiros-bubble-sort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.repeticao_exs4
+{
+    public class ex32_ordenar_inteiros_bubble_sort
+    {
+        public static void OrdenarInteirosBubbleSort()
+        {
+            try
+            {
+                Console.Write("Insira os elementos que entrarão no array (separados por vírgulas): ");
+                string[] entrada = Console.ReadLine().Split(',');
+                int[] array = entrada.Select(x => int.Parse(x.Trim())).ToArray();
+
+                if (array.Length == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("É necessário inserir algo para entrar no array");
+                    Console.ReadLine();
+                    return;
+                }
+
+                int n = array.Length;
+                int[] arrayOriginal = (int[])array.Clone();
+                for (int i = 0; i < n - 1; i++)
+                {
+                    bool repetir = false;
+
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
+                            repetir = true;
+                        }
+                    }
+
+                    if (!repetir)
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Array original: " + String.Join(", ", arrayOriginal) + "");
+                Console.WriteLine("Array ordenado com Bubble Sort: " + String.Join(", ", array) + "");
+                Console.ReadKey();
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("É possível inserir apenas valores numéricos");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- projeto-teste/testar-exs.cs
+++ projeto-teste/testar-exs.cs
@@ -19,7 +19,7 @@ namespace projeto
             //VendaEmpresa venda = new VendaEmpresa();
             //Menu_Principal.SelecionarMenu(funcionario, produto, venda);
 
-            repeticao_exs4.ex32_ordenar_strings_bubble_sort.OrdenarStringsBubbleSort();
+            repeticao_exs4.ex32_ordenar_inteiros_bubble_sort.OrdenarInteirosBubbleSort();
         }
     }
 }
